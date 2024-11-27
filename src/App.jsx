fad69b440750014a10a4fe4cd55fd990131ab5fa
Gab202-dev <bielgb202@gmail.com>
2024-11-27 19:41:25 -0300

    adicionei o footer e o querodoar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './components/Header/header'
 import'./global.scss'
+import Footer from './components/Footer/footer'
+
 
 
 export default function App(){
   return(
     <>
     <Header/>
+    <Footer/>
     </>
+
   )
 }
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,10 +1,23 @@
-
-
+import S from './queroDoar.module.scss'
+import Livro from '../../assets/Frame.png'
 
 export default function QueroDoar(){
     return(
-        <section>
-            <h2>Eu sou a QueroDoar</h2>
+        <section className={S.principal}  >
+           <section className={S.container}  >
+            <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+            <form action="">
+                <div>
+                    <img src={Livro} alt="" />
+                    <h3>Informações do Livro
+                    </h3>
+                </div>
+                <input type="text" placeholder='Titulo'  />
+                <input type="text" placeholder='Categoria' />
+                <input type="text" placeholder='Autor' />
+                <input type="text" placeholder='Link da Imagem' />
+            </form>
+           </section>
         </section>
     )
 }
