c2b2366ce71bf769991862e57dcef8e0217f3623
Gab202-dev <bielgb202@gmail.com>
2024-12-03 19:11:22 -0300

    deixando responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,28 @@
-
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
 export default function Doados(){
     return(
-        <section>
-            <h2>EU SOU A DOADOS</h2>
+        <section className={S.boxDoados}  >
+            <h2>Livros Doados</h2>
+            <section className={S.boxbooks}  >
+                <article>
+                  <img src={livro} alt="" />
+                  <h3>O Protagonista</h3>
+                  <p>Susanne Andrade
+                  </p>
+                  <p>Ficção</p>
+                </article>
+
+                <article>
+                  <img src={livro} alt="" />
+                  <h3>O Protagonista</h3>
+                  <p>Susanne Andrade
+                  </p>
+                  <p>Ficção</p>
+                </article>
+                
+            </section>
         </section>
     )
 }
