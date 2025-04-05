facfc7c91499dc3fc5b5e10330ab64d6e53a4e78
Gab202-dev <bielgb202@gmail.com>
2025-04-04 22:45:03 -0300

    adicionado a integração da API

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,27 +1,44 @@
 import S from './doados.module.scss'
 import livro from '../../assets/livro.png'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default function Doados(){
+
+  const [livros, setLivros] = useState([])
+
+
+  const getLivros = async () => {
+    try {
+        const response = await axios.get("https://api-nova-cnua.onrender.com/livros")
+        setLivros(response.data)
+    } catch (error) {
+        console.error("Erro ao buscar livros:", error)
+    }
+}
+
+
+useEffect(() => {
+  getLivros()
+}, [])
+
+
+
+
     return(
         <section className={S.boxDoados}  >
             <h2>Livros Doados</h2>
             <section className={S.boxbooks}  >
-                <article>
-                  <img src={livro} alt="" />
-                  <h3>O Protagonista</h3>
-                  <p>Susanne Andrade
-                  </p>
-                  <p>Ficção</p>
-                </article>
 
-                <article>
-                  <img src={livro} alt="" />
-                  <h3>O Protagonista</h3>
-                  <p>Susanne Andrade
-                  </p>
-                  <p>Ficção</p>
-                </article>
-                
+            {livros.map((item) => (
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
+
             </section>
         </section>
     )
